2fa9c8b9395ddb26a6798faddd032802a594babc
Dean Merchant <deanmerchant@gmail.com>
2015-09-06 17:53:42 +0100

    Better WILL example

--- client/Slides/WhatItLooksLike.js
+++ client/Slides/WhatItLooksLike.js
@@ -6,23 +6,39 @@ import Highlight from 'react-highlight';
 export default class WhatItLooksLike extends Component {
   render() {
     return (
-      <div className="text-slide">
+      <div className="code-slide">
         <Highlight className='javascript'>
           {`
-import React, { Component } from 'react';
-import HighLight from 'react-highlight';
+import React, { Component, PropTypes } from 'react';
+import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters } from '../actions';
+import AddTodo from '../components/AddTodo';
+import TodoList from '../components/TodoList';
+import Footer from '../components/Footer';
 
-export default class WhatItLooksLike extends Component {
+class App extends Component {
+  static propTypes = {
+    visibleTodos: PropTypes.arrayOf(PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired
+    })),
+    visibilityFilter: PropTypes.oneOf(['ALL', 'COMPLETED', 'ACTIVE']).isRequired
+  };
+
   render() {
     return (
-      <div className="text-slide">
-        <Highlight className='javascript'>
-          {"code snippet to be highlighted"}
-        </Highlight>
+      <div>
+        <h1>Todo List</h1>
+        <AddTodo onAddClick={ (text) => { addTodo(text); } } />
+        <TodoList todos={visibleTodos} onTodoClick={ (index) => { completeTodo(index); } } />
+        <Footer
+          filter={visibilityFilter}
+          onFilterChange={ (nextFilter) => { setVisibilityFilter(nextFilter); } }/>
       </div>
     );
   }
 }
+
+export default App
           `}
         </Highlight>
       </div>
