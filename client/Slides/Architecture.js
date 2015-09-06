91d3bedd9a7217d7e14e1f059c7b21df80fba5c4
Dean Merchant <deanmerchant@gmail.com>
2015-09-06 18:27:27 +0100

    Diagram

--- client/Slides/Architecture.js
+++ client/Slides/Architecture.js
@@ -1,3 +1,5 @@
+import '!style!css!../resources/styles/solarized_dark.css';
+import Highlight from 'react-highlight';
 import React, { Component } from 'react';
 
 export default class Architecture extends Component {
@@ -7,6 +9,23 @@ export default class Architecture extends Component {
         <div className="text-slide">
           <p>redux: http://rackt.github.io/redux/index.html</p>
           <p>redux = (state, action) => state</p>
+          <Highlight className='ascii' >
+          {`
+ _________               ____________              ___________
+|         |             |            |            |           |
+| Action  |------------>|  Reducer   |----------->|   State   |
+|_________|             |____________|            |___________|
+     ?                                                  |
+     |                                                  |
+     |                                                  |
+     |                                                  |
+     |                                                  |
+ ____|________                ___________               |
+|   User       |             |   React   |              |
+| interactions | <---------- |   Views   |<------------ |
+|______________|             |___________|
+          `}
+          </Highlight>
         </div>
       </div>
     );
